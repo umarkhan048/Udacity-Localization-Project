028a1655a82838a74d793b1d2844598fee8aea4f
Umar Khan <umarkhan048@yahoo.com>
2019-07-03 21:20:49 +0200

    particles for particle filter initialized

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -20,6 +20,7 @@
 
 using std::string;
 using std::vector;
+using std::normal_distribution;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
@@ -30,8 +31,26 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 1000;  // TODO: Set the number of particles
 
+  std::default_random_engine gen;
+
+  // Creates a normal (Gaussian) distribution for x
+  normal_distribution<double> dist_x(x, std[0]);
+  normal_distribution<double> dist_y(y, std[1]);
+  normal_distribution<double> dist_theta(theta, std[2]);
+
+  for (int i = 0; i < num_particles; i++){
+    Particle par;
+    par.id = i;
+    par.x = dist_x(gen);
+    par.y = dist_y(gen);
+    par.theta = dist_theta(gen);
+    par.weight = 1.0;
+   
+    particles.push_back(par);
+  }
+  is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
